86034410c440441dbe93b67a13dc5036efe21c17
TalIq501 <talhaiqbal501@gmail.com>
2025-01-21 10:43:32 +0500

    Original design created

---
+++ main.js
@@ -0,0 +1,15 @@
+class Book {
+    constructor (name, author, image, genre, dateAdded, datePublish) {
+        this.name = name;
+        this.author = author,
+        this.image = image,
+        this.genre = genre,
+        this.dateAdded = dateAdded,
+        this.datePublish = datePublish
+    }
+}
+
+const books = jsonData.books;
+
+
+
