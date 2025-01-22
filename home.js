b44cc467a5f58f35363d55b5a626adcf354639af
TalIq501 <talhaiqbal501@gmail.com>
2025-01-22 23:39:17 +0500

    Successfully made objects module

--- home.js
+++ home.js
@@ -1,50 +1,4 @@
-class Book {
-    constructor(name, author, language, image, genre, dateAdded, yearPublish) {
-        this.name = name;
-        this.author = author;
-        this.language = language
-        this.image = image;
-        this.genre = genre;
-        this.dateAdded = dateAdded;
-        this.yearPublish = yearPublish;
-    }
-
-    createBookElem = (obj) => {
-
-        const bookElem = document.createElement('div');
-        bookElem.setAttribute('id', this.name);
-        bookElem.classList.add('grid-object');
-        
-        const imgSpace = document.createElement('div');
-        imgSpace.classList.add('img-container');
-    
-        const img = document.createElement('img');
-        img.src = this.image;
-    
-        const bottomBar = document.createElement('div');
-        bottomBar.classList.add('bottom-bar');
-    
-        const dataBar = document.createElement('div');
-        dataBar.classList.add('data-bar');
-    
-        const bookNameDisp = document.createElement('div');
-        bookNameDisp.classList.add('book-name-display');
-        bookNameDisp.textContent = this.name;
-    
-        const bookAuthorDisp = document.createElement('div');
-        bookAuthorDisp.classList.add('book-author-display');
-        bookAuthorDisp.textContent = this.author;
-    
-        imgSpace.appendChild(img)
-        bookElem.appendChild(imgSpace);
-        bookElem.appendChild(bottomBar);
-        bottomBar.appendChild(dataBar);
-        dataBar.appendChild(bookNameDisp);
-        dataBar.appendChild(bookAuthorDisp);
-    
-        obj.appendChild(bookElem);
-    };
-}
+import { Book } from './modules/objects.js';
 
 const booksDiv = document.getElementById('recs-grid-books')
 
@@ -80,7 +34,6 @@ const createBookDisplay = (n, books) => {
             book["date_added"],
             book["date_published"]
         );
-    
     
         books.push(newBook);
     });
