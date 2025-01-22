5c554e01289a703143d5209920b034898dd2f999
TalIq501 <talhaiqbal501@gmail.com>
2025-01-22 21:31:50 +0500

    Books page functioning

--- pages/booksPage/books.js
+++ pages/booksPage/books.js
@@ -8,13 +8,49 @@ class Book {
         this.dateAdded = dateAdded;
         this.yearPublish = yearPublish;
     }
+
+    createBookElem = (obj) => {
+
+        const bookElem = document.createElement('div');
+        bookElem.setAttribute('id', this.name);
+        bookElem.classList.add('grid-object');
+        
+        const imgSpace = document.createElement('div');
+        imgSpace.classList.add('img-container');
+    
+        const img = document.createElement('img');
+        img.src = this.image;
+    
+        const bottomBar = document.createElement('div');
+        bottomBar.classList.add('bottom-bar');
+    
+        const dataBar = document.createElement('div');
+        dataBar.classList.add('data-bar');
+    
+        const bookNameDisp = document.createElement('div');
+        bookNameDisp.classList.add('book-name-display');
+        bookNameDisp.textContent = this.name;
+    
+        const bookAuthorDisp = document.createElement('div');
+        bookAuthorDisp.classList.add('book-author-display');
+        bookAuthorDisp.textContent = this.author;
+    
+        imgSpace.appendChild(img)
+        bookElem.appendChild(imgSpace);
+        bookElem.appendChild(bottomBar);
+        bottomBar.appendChild(dataBar);
+        dataBar.appendChild(bookNameDisp);
+        dataBar.appendChild(bookAuthorDisp);
+    
+        obj.appendChild(bookElem);
+    };
 }
 
-const booksDiv = document.getElementById('recs-grid-books')
+const booksDiv = document.getElementById('grid-books')
 
 async function fetchData() {
     try {
-        const res = await fetch('./data/db.json');
+        const res = await fetch('/data/db.json');
         const data = await res.json();
         return data;
     } catch (err) {
@@ -22,47 +58,9 @@ async function fetchData() {
     }
 }
 
-const createBookElem = (obj) => {
-
-    const bookElem = document.createElement('div');
-    bookElem.setAttribute('id', obj.name);
-    bookElem.classList.add('grid-object');
-    
-    const imgSpace = document.createElement('div');
-    imgSpace.classList.add('img-container');
-
-    const img = document.createElement('img');
-    img.src = obj.image;
-
-    const bottomBar = document.createElement('div');
-    bottomBar.classList.add('bottom-bar');
-
-    const dataBar = document.createElement('div');
-    dataBar.classList.add('data-bar');
-
-    const bookNameDisp = document.createElement('div');
-    bookNameDisp.classList.add('book-name-display');
-    bookNameDisp.textContent = obj.name;
-
-    const bookAuthorDisp = document.createElement('div');
-    bookAuthorDisp.classList.add('book-author-display');
-    bookAuthorDisp.textContent = obj.author;
-
-    imgSpace.appendChild(img)
-    bookElem.appendChild(imgSpace);
-    bookElem.appendChild(bottomBar);
-    bottomBar.appendChild(dataBar);
-    dataBar.appendChild(bookNameDisp);
-    dataBar.appendChild(bookAuthorDisp);
-
-    recsBooks.appendChild(bookElem);
-};
-
-const createBookDisplay = (n, books) => {
-    const bookDisplayList = books.slice(0, n)
-
-    bookDisplayList.forEach(book => {
-        createBookElem(book)
+const createBookDisplay = (books) => {
+    books.forEach(book => {
+        book.createBookElem(booksDiv)
     })
 };
 
@@ -80,13 +78,12 @@ const createBookDisplay = (n, books) => {
             book["date_added"],
             book["date_published"]
         );
-    
-    
+        
         books.push(newBook);
     });
 
     if (books) {
-        createBookDisplay(4, books);
+        createBookDisplay(books);
     } else {
         console.log('Data is incorrect');
         console.log(typeof data);
