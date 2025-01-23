76316b81ef625926379ddf4e51a0b7a5fbd59bf7
TalIq501 <talhaiqbal501@gmail.com>
2025-01-23 11:20:35 +0500

    Finalised bubble sort

--- modules/utils.js
+++ modules/utils.js
@@ -1,18 +1,22 @@
-const bubbleSort = (arr, length) => {
+export const bubbleSort = (arr, length, param) => {
     for (let i = 0; i < length - 1; i++) {
         for (let j = 0; j < length - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                let temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+            if (param) {
+                if (arr[j][param] > arr[j + 1][param]) {
+                    let temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            } else {
+                if (arr[j] > arr[j + 1]) {
+                    let temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
             }
         }
     }
 
     return arr
 }
-
-const u = [3, 5, 9, 4, 7]
-
-console.log(bubbleSort(u, u.length));
 
